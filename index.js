3856c69353f6987155ace0b032ae53a53d543b2c
Stone Mitchell <stonemitchell95@gmail.com>
2018-03-15 18:42:33 -0400

    Final Project

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ $(document).ready(function(){
     });
 
  $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+function sayThanks(){
+  event.preventDefault();
+  var sign_up = document.querySelector("#sign_up");
+  console.log("sign_up", sign_up )
+  sign_up.innerHTML = "";
+  var h1 = document.createElement("h1");
+  h1.id = "Thanks"
+  h1.innerText = "Thank You!"
+  console.log("h1", h1)
+  // h1.style.margin= "0 auto";
+  document.querySelector("main").appendChild(h1);
+
+}
+
+
+
+function sayThanks2(){
+  event.preventDefault();
+  var choice = document.querySelector("#choice");
+  console.log("choice", choice )
+  choice.innerHTML = "";
+  var h5 = document.createElement("h5");
+  h5.id = "Thanks2"
+  h5.innerText = "Thank You!"
+  console.log("h5", h5)
+  // h1.style.margin= "0 auto";
+  document.querySelector("main").appendChild(h5);
+}
